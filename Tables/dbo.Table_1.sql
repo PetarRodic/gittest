959457779793d77e8bda918b8d9b809e23002ead
petarrodic <>
2014-12-23 14:12:48 +0100

    gdfg

--- Tables/dbo.Table_1.sql
+++ Tables/dbo.Table_1.sql
@@ -3,15 +3,16 @@ SET QUOTED_IDENTIFIER ON
 SET ANSI_PADDING ON
 GO
 CREATE TABLE [dbo].[Table_1] (
-		[ertet]       [nchar](10) COLLATE Latin1_General_CI_AI NULL,
-		[dfgdfg]      [int] NULL,
-		[ascasas]     [nchar](10) COLLATE Latin1_General_CI_AI NULL,
-		[fsdf]        [numeric](18, 0) NULL,
-		[xcvxv]       [nchar](10) COLLATE Latin1_General_CI_AI NULL,
-		[bcvbcb]      [nchar](10) COLLATE Latin1_General_CI_AI NULL,
-		[zxczc]       [nchar](10) COLLATE Latin1_General_CI_AI NULL,
-		[czcx]        [int] NULL
-) ON [PRIMARY]
+		[prva]         [nchar](10) COLLATE Latin1_General_CI_AI NULL,
+		[druga]        [nchar](10) COLLATE Latin1_General_CI_AI NULL,
+		[treca]        [nvarchar](max) COLLATE Latin1_General_CI_AI NULL,
+		[cetvrta]      [nchar](10) COLLATE Latin1_General_CI_AI NULL,
+		[peta]         [int] NULL,
+		[666]          [int] NULL,
+		[sssss]        [nchar](10) COLLATE Latin1_General_CI_AI NULL,
+		[ffffffff]     [nchar](10) COLLATE Latin1_General_CI_AI NULL,
+		[dd]           [nchar](10) COLLATE Latin1_General_CI_AI NULL
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[Table_1] SET (LOCK_ESCALATION = TABLE)
 GO
